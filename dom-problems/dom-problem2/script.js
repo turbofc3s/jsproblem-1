cfb59074819c5cefbbe4c3a886c0d192f0b62489
jeffrey burke <burkejeffrey56@yahoo.com>
2021-12-08 19:19:06 -0500

    finished dom problem 2

--- dom-problems/dom-problem2/script.js
+++ dom-problems/dom-problem2/script.js
@@ -1,4 +1,4 @@
-/* 1. tell the computer to use the getElementById method to find the link attribute on the page
+ /* 1. tell the computer to use the getElementById method to find the link attribute on the page
 2.then use the getAttribute to target the href attribute
 3. use innerHTML method on p2 to grab the information in the element 
 4. use the mybtn id and the onclick attribute (getHref()) to get the attribute of the link
@@ -6,7 +6,7 @@
 */
 function getHref(){
 	const linkUrl = document.getElementById("link").getAttribute("href");
-	document.getElementById("p2").innerhtml = linkUrl;
-	document.getElementById('mybtn').getAttribute("onclick");
+	document.getElementById("p2").innerHTML = linkUrl;
+	
 
 }
