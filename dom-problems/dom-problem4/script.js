fc3fbb9a2b93e1a1cdb2b3ed631b321f17920318
jeffrey burke <burkejeffrey56@yahoo.com>
2021-12-28 09:53:47 -0500

    second commit of problem 4

--- dom-problems/dom-problem4/script.js
+++ dom-problems/dom-problem4/script.js
@@ -2,19 +2,25 @@
 three items in it, then write a JavaScript function that 
 displays the number of items and displays the item in the list.
 */
-;		
+		
 
-function allTheDrinks(bigList) {
-let arrayOfOptions = bigList.options;
-console.log(arrayOfOptions);
-  alert("I am an alert box!");
-  let x = document.getElementById("bigList").value;
-  document.getElementById("p1").innerHTML = "You selected: " + x;
+function allTheDrinks() {
+	let arrayOfOptions = document.getElementById("drinks").options;
+	let strOfOptions = "";
+	console.log(arrayOfOptions.length);
 
-	return bigList.options.length;
+for (let i = 0; i < arrayOfOptions.length; i++) {
+	console.log(arrayOfOptions[i].value);
+
+ strOfOptions + arrayOfOptions[i].value + " "
+  
+  
+
+
+  
+  console.log()
+
+
 }
-let theDrinks = document.getElementById("drinks");		
-console.log(allTheDrinks(theDrinks));
-
 
 }
