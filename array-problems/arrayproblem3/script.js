5f002661ac88ca78598ec0a38b975576e9c50a83
jeffrey burke <burkejeffrey56@yahoo.com>
2022-01-18 20:29:18 -0500

    initial commit

---
+++ array-problems/arrayproblem3/script.js
@@ -0,0 +1,14 @@
+/*
+Define a function that takes in an array and returns the sorted version of that array.
+
+*/
+
+let initialArray = ["sun", "moon", "stars", "sky"];
+
+function sortedArray(arry) {
+	 let newArray = arry.sort();
+
+    return newArray;
+
+}
+console.log(sortedArray(initialArray));
