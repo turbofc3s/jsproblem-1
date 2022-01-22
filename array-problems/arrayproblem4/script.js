928e83ff3f9b115cf34e36a7173f0579eeb40c44
jeffrey burke <burkejeffrey56@yahoo.com>
2022-01-22 18:06:50 -0500

    finished the problem

---
+++ array-problems/arrayproblem4/script.js
@@ -0,0 +1,24 @@
+/*
+Write a JavaScript function to remove duplicate items from an array 
+(consider case sensitivity).
+Example array: [1,2,2,4,5,4,7,8,7,3,6]
+NOTE: Do NOT use the Set method.
+1. define function that takes in an array as a paramenter
+2. set up for each loop to check for duplicae the items in the array
+3. remove duplicate items from array into new array
+*/
+let girls = ["becky", "dana", "becky", "Monica", "rachel","brandy"]  
+
+function myGirls (arry) {
+	let newOne = [];  
+	arry.forEach(item => {  
+   		if (!newOne.includes(item)) {  
+      		newOne.push(item);        	
+      }
+   });
+   return newOne;
+   }  
+console.log(myGirls(girls));  
+
+    
+
