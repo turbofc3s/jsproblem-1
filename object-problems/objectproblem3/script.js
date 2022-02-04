9ffbb4d99b494697ffd749a48025a3113ffe475d
jeffrey burke <burkejeffrey56@yahoo.com>
2022-02-03 20:34:23 -0500

    intial commit

---
+++ object-problems/objectproblem3/script.js
@@ -0,0 +1,19 @@
+/* Create an object with three items in it, then write a JavaScript function 
+that accepts an object and tells you the length (tells you how many items are in it) of the object.
+*/
+
+let threeObjects = {
+	haircolor: "blue",
+	eyecolor: "green",
+	type: "new",
+    age: 42,
+    shape: "round"
+    
+};
+
+function lengthOfObject (obj) {
+         objectLength = Object.keys(obj).length
+         return objectLength
+
+};
+console.log(lengthOfObject(threeObjects))
